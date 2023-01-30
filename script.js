75e9518561d919422ed64dcc439da098acf4cf70
maxmisling <60746519+maxmisling@users.noreply.github.com>
2023-01-30 22:17:25 +0700

    Update script.js
    set correct beaconactivation date

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // counting days to new year
-const countDownDateTime = new Date(2023, 1, 1, 0, 0, 0).getTime();
+const countDownDateTime = new Date(2023, 2, 17, 21, 20, 27).getTime();
 const daysValue = document.querySelector("#days");
 const hoursValue = document.querySelector("#hours");
 const minutesValue = document.querySelector("#minutes");
